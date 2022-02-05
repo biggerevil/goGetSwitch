b9bdd0def2aacbe8462980341d0e550bfe570b30
biggerevil <tugunrus@icloud.com>
2022-02-05 10:50:31 +0300

    Как передавать комбинации для получения stats, без powerset

--- testMain.go
+++ testMain.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"goGetSwitch/dbFunctions"
 	"goGetSwitch/producerCode"
+	"goGetSwitch/stats"
 )
 
 /*
@@ -11,9 +13,23 @@ import (
 */
 
 func main() {
-	powerset, _ := producerCode.GeneratePowersetWithinBorders(554084335616, 554084335618)
-	for index, value := range powerset {
-		fmt.Println("Combination #", index, " = ", value)
-	}
-	//fmt.Println("powerset = ", powerset)
+	// 277042167809
+	// 554084335617
+	//powerset, _ := producerCode.GeneratePowersetWithinBorders(277042167809, 277042167815)
+	//for index, value := range powerset {
+	//	fmt.Println("Combination #", index, " = ", value)
+	//}
+
+	var combination producerCode.Combination
+
+	combination.Conditions = append(combination.Conditions, producerCode.Condition{"Pairname", "AUD/USD"})
+	combination.Conditions = append(combination.Conditions, producerCode.Condition{"Timeframe", "300"})
+
+	fmt.Println("combination = ", combination)
+
+	collection := dbFunctions.ConnectToDB()
+	statsOfCombination := dbFunctions.GetCombinationStats(combination, collection)
+
+	//fmt.Println("stats = ", stats)
+	stats.PrintStats(statsOfCombination)
 }
